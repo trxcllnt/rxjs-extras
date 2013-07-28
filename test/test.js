0629736a8696e89e2cf87153dd49ee8eb63b9507
paul taylor <paul.e.taylor@me.com>
2013-07-27 19:13:49 -0500

    Moves rx.easing into its own npm project.

--- test/test.js
+++ test/test.js
@@ -1,7 +1,2 @@
 var Rx = require('rx');
-require('../lib/rx');
-require('../lib/rx.easing');
-
-Rx.Observable.backIn(0, 1000, 2431).subscribe(function(val) {
-	console.log(val);
-});
+require('../lib/rx.extras');
